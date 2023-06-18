c834e94a55c121c1c9df461e27825d05dd0ffa03
Konrad <konrad.wojtylo.9@gmail.com>
2023-06-18 12:18:52 +0200

    added hover to nav li, changed buttons font size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,20 +16,20 @@ const Navbar = () => {
 			</div>
 			<ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
 				{['Item', 'Item', 'Item'].map((item, index) => (
-					<NavbarItem key={item + index} title={item} />
+					<NavbarItem key={item + index} title={item} classProps={'hover:text-red-700 transition duration-300'}/>
 				))}
 				<li className='bg-[#b91c1c] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#7f1d1d] transition duration-300'>Download</li>
 			</ul>
 			<div className='flex relative'>
 				{toggleMenu ? (
 					<AiOutlineClose
-						fontSize={28}
+						fontSize={32}
 						className='text-white md:hidden cursor-pointer'
 						onClick={() => setToggleMenu(false)}
 					/>
 				) : (
 					<HiMenuAlt4
-						fontSize={28}
+						fontSize={32}
 						className='text-white md:hidden cursor-pointer'
 						onClick={() => setToggleMenu(true)}
 					/>
@@ -37,7 +37,7 @@ const Navbar = () => {
 				{toggleMenu && (
 					<ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md text-white bg-black'>
 						<li className='text-xl self-start p-3 my-2 cursor-pointer'>
-							<AiOutlineClose onClick={() => setToggleMenu(false)} />
+							<AiOutlineClose fontSize={28} onClick={() => setToggleMenu(false)} />
 						</li>
 						{['Item', 'Item', 'Item'].map((item, index) => (
 							<NavbarItem key={item + index} title={item} classProps={'my-2 text-lg self-center'} />
