5f01d4d7d0684af3138fa40c3a87c992c6429c5d
Konrad <konrad.wojtylo.9@gmail.com>
2023-06-20 23:30:07 +0200

    created footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,25 @@
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
 const Footer = () => {
-    return (
-        <h1>Footer</h1>
-    )
+
+    const currentYear = new Date().getFullYear()
+
+	return (
+		<div className='w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer text-white'>
+			<div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-20' />
+			<div className='sm:w-[90%] w-full flex justify-between items-center my-5'>
+				<p className='text-sm text-center mf:text-base'>&copy; {currentYear} Test Title</p>
+				<div className='flex flex-end '>
+					<a className='cursor-pointer mr-8' href='https://github.com/' target='_blank' rel='noreferrer'>
+						<BsGithub fontSize={22} className='hover:text-red-800 transition duration-300' />
+					</a>
+					<a className='cursor-pointer' href='https://pl.linkedin.com/' target='_blank' rel='noreferrer'>
+						<BsLinkedin fontSize={22} className='hover:text-red-800 transition duration-300' />
+					</a>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Footer
