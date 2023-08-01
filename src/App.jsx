48e2c160c30a9ded7de16d639f84bde79b109492
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-01 19:53:27 +0200

    Made some improves before release v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import { Navbar, Welcome, Footer, Extras, Contact } from "./sections"
+import { Route, Routes } from 'react-router-dom'
+import { Navbar, Welcome, Footer, Extras, Contact } from './sections'
 
 const App = () => {
-  return (
-    <div className='min-h-screen'>
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Welcome />}/>
-          <Route path="/contact" element={<Contact />}/>
-        </Routes>
-      </div>
-        <Extras />
-        <Footer />
-    </div>
-  )
+	return (
+		<div className='min-h-screen'>
+			<div className='gradient-bg-welcome'>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Welcome />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</div>
+			<Extras />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
