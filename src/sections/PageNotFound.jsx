7225c957f86c0b796ecfd655087315767dbfbb97
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-21 11:02:02 +0200

    Moved back svg to public due to production bug

--- src/sections/PageNotFound.jsx
+++ src/sections/PageNotFound.jsx
@@ -2,7 +2,7 @@ const PageNotFound = () => {
 	return (
 		<div className='flex w-full justify-center items-center min-h-[600px] h-screen p-5'>
 			<div className='flex flex-col items-center justify-between md:p-20 py-16 px-4'>
-				<img src='/src/images/notfound-img.svg' alt='Error-404 image' className='w-[600px]' />
+				<img src='/notfound-img.svg' alt='Error-404 image' className='w-[600px]' />
 				<p className='text-white text-xs'>
 					Error 404 image by{' '}
 					<a
