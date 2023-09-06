3f79c01a741c70c154ca0d5f39713a1c13a14f88
Konrad <konrad.wojtylo.9@gmail.com>
2023-09-06 17:54:25 +0200

    Added second commercial project to welcome section

--- src/data/CardData.jsx
+++ src/data/CardData.jsx
@@ -4,15 +4,14 @@ const CardData = [
 		title: 'Nietypowa-Sowa',
 		countdown: 1692201600000,
 		webHandle: 'https://nietypowa-sowa.pl/',
-		text: 'Site of an artist who works extensively in painting. The site has a contact form along with reCaptcha. All made using React.',
+		text: 'Site of an artist who works extensively in painting. The site has a contact form along with reCaptcha. All done using React.',
 	},
 	{
 		id: 2,
 		title: 'Saints Arch&Dev',
 		countdown: 1694012400000,
-		webHandle: '',
-		text: '',
-		specialText: '*Reschedule due to some major changes',
+		webHandle: 'https://saints.com.pl/',
+		text: 'Site of a real estate office operating virtually in GTA Online. The site has sub-pages, built using useParams(). All done using React.',
 	},
 	{
 		id: 3,
