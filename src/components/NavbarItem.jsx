c668c783cae00c465bc0785956462b567746aa9b
Konrad <konrad.wojtylo.9@gmail.com>
2023-09-11 15:13:50 +0200

    Changed NavbarItem otherProps name to be more clear

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,10 +1,10 @@
 import { NavLink } from 'react-router-dom'
 
-export const NavbarItem = ({ title, section, classProps, onClick, ...otherProps }) => {
+export const NavbarItem = ({ title, section, classProps, onClick, ...linkProps }) => {
 	return (
 		<li
 			className={`mx-2 mf:mx-4 cursor-pointer hover:text-red-500 transition duration-300 ${classProps} uppercase z-10`}>
-			<NavLink className='p-2' to={section} onClick={onClick} {...otherProps}>
+			<NavLink className='p-2' to={section} onClick={onClick} {...linkProps}>
 				{title}
 			</NavLink>
 		</li>
