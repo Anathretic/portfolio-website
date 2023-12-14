a0db922f2a004fb8345eaebb5e64515e95b441cc
Konrad <konrad.wojtylo.9@gmail.com>
2023-12-14 20:57:47 +0100

    Fixed page structure

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<div className='w-full flex md:justify-center justify-between items-center flex-col py-4 text-white'>
+		<footer className='w-full flex md:justify-center justify-between items-center flex-col py-4 text-white'>
 			<div className='sm:w-[90%] w-full h-[1px] bg-gray-400 mt-20' />
 			<div className='sm:w-[90%] w-full flex justify-between items-center my-5 rrsm:px-2 mf:px-0'>
 				<div className='flex'>
@@ -29,7 +29,7 @@ const Footer = () => {
 					</a>
 				</div>
 			</div>
-		</div>
+		</footer>
 	);
 };
 
