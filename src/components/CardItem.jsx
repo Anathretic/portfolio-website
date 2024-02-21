2297b0bac29d1b93687dad1f1174e0bb712de3ef
Konrad <konrad.wojtylo.9@gmail.com>
2024-02-21 16:33:02 +0100

    Changed background image and cards images, made some minor CSS changes, changed card component a little

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -10,7 +10,7 @@ const defaultRemainingTime = {
 	days: '00',
 };
 
-export const CardItem = ({ title, webHandle, gitHandle, countdown, text, imageID }) => {
+export const CardItem = ({ webHandle, gitHandle, countdown, text, imageID }) => {
 	const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
 
 	const updateRemainingTime = countdown => {
@@ -27,21 +27,18 @@ export const CardItem = ({ title, webHandle, gitHandle, countdown, text, imageID
 	return (
 		<div className='w-full max-sm:max-w-[350px]'>
 			<div
-				className={`p-3 flex justify-end items-end flex-col rounded-xl h-60 sm:w-96 my-3 ${
+				className={`p-3 flex items-end flex-col rounded-xl h-60 sm:w-96 my-3 ${
 					webHandle ? `card-${imageID}` : 'card animate-pulse'
 				}`}>
 				{webHandle && (
 					<a
-						className='w-12 h-14 rounded-full border-2 border-white flex justify-center items-center text-white hover:bg-[#b91c1c] transition duration-300'
+						className='w-12 h-12 rounded-full border-2 border-white flex justify-center items-center text-white hover:bg-[#b91c1c] transition duration-300'
 						href={webHandle}
 						target='_blank'
 						rel='noreferrer'>
 						<FiChevronsRight fontSize={28} />
 					</a>
 				)}
-				<div className='flex w-full h-full'>
-					<p className='text-white font-light text-lg self-end'>{title}</p>
-				</div>
 			</div>
 			<div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-gradient text-white'>
 				<div>
