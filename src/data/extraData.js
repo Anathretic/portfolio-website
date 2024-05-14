a83df562035be9467dc4efc1bdb517a4030b9c1c
Konrad <konrad.wojtylo.9@gmail.com>
2024-05-14 11:29:56 +0200

    Updated extraData file

--- src/data/extraData.js
+++ src/data/extraData.js
@@ -1,6 +1,5 @@
 import Globe from '../images/icons/globe.svg';
 import Note from '../images/icons/note.svg';
-import StarWars from '../images/icons/star-wars.svg';
 
 export const extraData = [
 	{
@@ -18,13 +17,5 @@ export const extraData = [
 		subtitle:
 			'Web application that allows you to create notes. The whole thing works in sync with firebase and all data is stored in a NoSQL database. Feel free to use!',
 		href: 'https://noteapp.online/',
-	},
-	{
-		id: 6,
-		icon: StarWars,
-		title: 'Star Wars & Calculator',
-		subtitle:
-			'A demonstration of the use of Next.js with Cypress and SWR for data fetching. It is split into 2 parts, allowing the use of component testing as well as E2E tests.',
-		href: 'https://star-wars-and-calculator-app.vercel.app/',
 	},
 ];
