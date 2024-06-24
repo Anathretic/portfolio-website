3702d58182d91b767902fab71672161944973092
Konrad <konrad.wojtylo.9@gmail.com>
2024-06-24 20:12:00 +0200

    Updated extra items links

--- src/data/extraData.js
+++ src/data/extraData.js
@@ -8,7 +8,7 @@ export const extraData = [
 		title: 'Countrypedia',
 		subtitle:
 			'Simple app built on a REST API to check information about a country you are interested in. For the sake of your eyes - a dark mode has been added!',
-		href: 'https://countrypedia.online/',
+		href: 'https://countrypedia-project.vercel.app/',
 	},
 	{
 		id: 5,
@@ -16,6 +16,6 @@ export const extraData = [
 		title: 'NoteApp',
 		subtitle:
 			'Web application that allows you to create notes. The whole thing works in sync with firebase and all data is stored in a NoSQL database. Feel free to use!',
-		href: 'https://noteapp.online/',
+		href: 'https://with-firebase-note-app.vercel.app/',
 	},
 ];
