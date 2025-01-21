7316b2dfb1b38f17eef29542fbd8021909bb94a4
Konrad <konrad.wojtylo.9@gmail.com>
2025-01-21 23:59:10 +0100

    Fixed navbar animation and improved structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { NavbarAnchorItem, NavbarHashItem } from './components/NavbarItem';
 import { navbarItems } from './navbarItems/navbarItems';
 import { AiOutlineClose } from 'react-icons/ai';
@@ -8,7 +9,23 @@ import { scrollToTop } from '../../utils/scrollToTop';
 
 const Navbar = () => {
 	const [toggleMenu, setToggleMenu] = useState(false);
+	const [isAnimating, setIsAnimating] = useState(false);
 	const [isScrolled, setIsScrolled] = useState(false);
+
+	const isMobile = useMediaQuery({ query: '(max-width: 990px)' });
+
+	const handleMenuClose = () => {
+		setIsAnimating(true);
+		setToggleMenu(false);
+
+		setTimeout(() => {
+			setIsAnimating(false);
+		}, 300);
+	};
+
+	const handleAnimationEnd = () => {
+		setIsAnimating(false);
+	};
 
 	const handleScroll = () => {
 		if (window.scrollY > 60) {
@@ -36,64 +53,65 @@ const Navbar = () => {
 					</div>
 				</div>
 				<nav>
-					<ul className='text-white mf:flex hidden list-none flex-row justify-between items-center flex-initial'>
-						{navbarItems.map(({ title, section }) => (
-							<NavbarHashItem key={title} title={title} section={section} />
-						))}
-						<NavbarAnchorItem section='/privacy-policy' title='privacy' onClick={scrollToTop} />
-						<li className='ml-4'>
-							<a
-								className='p-1 hover:text-red-500 transition duration-300'
-								href='https://www.linkedin.com/in/konrad-wojtylo'
-								target='_blank'
-								rel='noreferrer'>
-								<BsLinkedin fontSize={22} />
-							</a>
-						</li>
-					</ul>
-				</nav>
-				<div className='flex relative'>
-					<HiMenuAlt4
-						fontSize={32}
-						className='text-white mf:hidden block cursor-pointer'
-						onClick={() => setToggleMenu(true)}
-					/>
-					{toggleMenu && (
-						<nav>
-							<ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl mf:hidden list-none flex flex-col justify-start items-center rounded-md text-white bg-[#141d3bfb] animate-slide-in'>
-								<li className='text-xl self-start my-2 cursor-pointer'>
-									<AiOutlineClose fontSize={28} onClick={() => setToggleMenu(false)} />
-								</li>
-								{navbarItems.map(({ title, section }) => (
-									<NavbarHashItem
-										key={title}
-										title={title}
-										section={section}
-										classProps={'mb-5'}
-										onClick={() => setToggleMenu(false)}
+					{isMobile ? (
+						<>
+							<HiMenuAlt4 fontSize={32} className='text-white cursor-pointer' onClick={() => setToggleMenu(true)} />
+							{(toggleMenu || isAnimating) && (
+								<ul
+									onAnimationEnd={handleAnimationEnd}
+									className={`z-10 fixed top-1 right-1 bottom-1 p-3 w-[80vw] min-[500px]:w-[70vw] shadow-2xl list-none flex flex-col justify-start items-center rounded-md text-white bg-[#141d3bfb] overflow-y-auto ${
+										toggleMenu ? 'animate-slide-in' : 'animate-slide-out'
+									}`}>
+									<li className='text-xl self-start my-2 cursor-pointer'>
+										<AiOutlineClose fontSize={28} onClick={handleMenuClose} />
+									</li>
+									{navbarItems.map(({ title, section }) => (
+										<NavbarHashItem
+											key={title}
+											title={title}
+											section={section}
+											classProps={'mb-5'}
+											onClick={handleMenuClose}
+										/>
+									))}
+									<NavbarAnchorItem
+										section='/privacy-policy'
+										title='privacy'
+										onClick={() => {
+											handleMenuClose();
+											scrollToTop();
+										}}
 									/>
-								))}
-								<NavbarAnchorItem
-									section='/privacy-policy'
-									title='privacy'
-									onClick={() => {
-										setToggleMenu(false);
-										scrollToTop();
-									}}
-								/>
-								<li>
-									<a
-										className='p-1 hover:text-red-500 transition duration-300'
-										href='https://www.linkedin.com/in/konrad-wojtylo'
-										target='_blank'
-										rel='noreferrer'>
-										<BsLinkedin fontSize={22} />
-									</a>
-								</li>
-							</ul>
-						</nav>
+									<li>
+										<a
+											className='p-1 hover:text-red-500 transition duration-300'
+											href='https://www.linkedin.com/in/konrad-wojtylo'
+											target='_blank'
+											rel='noreferrer'>
+											<BsLinkedin fontSize={22} />
+										</a>
+									</li>
+								</ul>
+							)}
+						</>
+					) : (
+						<ul className='text-white flex list-none flex-row justify-between items-center flex-initial'>
+							{navbarItems.map(({ title, section }) => (
+								<NavbarHashItem key={title} title={title} section={section} />
+							))}
+							<NavbarAnchorItem section='/privacy-policy' title='privacy' onClick={scrollToTop} />
+							<li className='ml-4'>
+								<a
+									className='p-1 hover:text-red-500 transition duration-300'
+									href='https://www.linkedin.com/in/konrad-wojtylo'
+									target='_blank'
+									rel='noreferrer'>
+									<BsLinkedin fontSize={22} />
+								</a>
+							</li>
+						</ul>
 					)}
-				</div>
+				</nav>
 			</div>
 		</header>
 	);
